57544e08bfe7ab5bb2195754f4f2673b237e4c19
PalumboN <nahuel.palumbo@gmail.com>
2019-03-16 00:50:15 -0300

    Siempre arriba

--- ai.js
+++ ai.js
@@ -25,7 +25,7 @@ function Ai() {
     //              This changes the state of the grid object, so you should probably copy() the grid before using this.
     //              Naturally the modified state doesn't contain information about new tiles.
     //              Method returns true if you can move to that direction, false otherwise.
-
+    console.log(grid.move(0))
     return 0;
   }
 }
